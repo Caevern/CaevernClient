74cf7b0f3a6b721e42a3452ea761cf1b770939ed
Charli <123391187+CharliVanNood@users.noreply.github.com>
2026-07-25 22:47:49 +0200

    renamed static textures to avoid conflicting textures

--- src/world/world.rs
+++ src/world/world.rs
@@ -2,20 +2,28 @@ use std::collections::{HashMap, HashSet};
 
 use cgmath::Vector3;
 
-use crate::{renderer::{transform::Transform, vertex::create_vertices_skinned}, world::{material::Material, object::{Object, ObjectType}, objects::fbx_parser::parse, scene::load_scene}};
+use crate::{
+    renderer::{transform::Transform, vertex::create_vertices_skinned},
+    world::{
+        material::Material,
+        object::{Object, ObjectType},
+        objects::fbx_parser::parse,
+        scene::load_scene,
+    },
+};
 
 #[derive(Clone)]
 pub struct World {
     pub objects: Vec<Object>,
     pub textures: HashSet<String>,
-    cameras: Vec<Object>
+    cameras: Vec<Object>,
 }
 impl World {
-    pub fn new() -> Self{
+    pub fn new() -> Self {
         Self {
             objects: Vec::new(),
             textures: HashSet::new(),
-            cameras: Vec::new()
+            cameras: Vec::new(),
         }
     }
 
@@ -48,28 +56,44 @@ impl World {
     pub fn load_world(&mut self, path: &str) {
         let scene = load_scene(path);
         if let Ok(scene) = scene {
-            let mut static_world_object = Object::create(
-                ObjectType::StaticMesh,
-                Vec::new()
-            );
+            let mut static_world_object = Object::create(ObjectType::StaticMesh, Vec::new());
 
             for scene_object in scene.objects {
                 if scene_object.is_static {
-                    let model_parsed = parse(&scene_object.reference,
+                    let model_parsed = parse(
+                        &scene_object.reference,
                         Transform::new(
-                            Vector3::new(scene_object.transform.position[0], scene_object.transform.position[1], scene_object.transform.position[2]),
-                            Vector3::new(scene_object.transform.rotation[0], scene_object.transform.rotation[1], scene_object.transform.rotation[2]),
-                            Vector3::new(scene_object.transform.scale[0], scene_object.transform.scale[1], scene_object.transform.scale[2])
-                        )
+                            Vector3::new(
+                                scene_object.transform.position[0],
+                                scene_object.transform.position[1],
+                                scene_object.transform.position[2],
+                            ),
+                            Vector3::new(
+                                scene_object.transform.rotation[0],
+                                scene_object.transform.rotation[1],
+                                scene_object.transform.rotation[2],
+                            ),
+                            Vector3::new(
+                                scene_object.transform.scale[0],
+                                scene_object.transform.scale[1],
+                                scene_object.transform.scale[2],
+                            ),
+                        ),
                     );
 
                     let mut vertices = create_vertices_skinned(&model_parsed.0);
                     for mesh in &mut vertices {
                         if mesh.1 == "default" {
-                            mesh.1 = format!("material{}", static_world_object.get_materials().len());
+                            mesh.1 = format!(
+                                "caevern.material{}",
+                                static_world_object.get_materials().len()
+                            );
                         }
                     }
-                    let material_name = format!("material{}", static_world_object.get_materials().len());
+                    let material_name = format!(
+                        "caevern.material{}",
+                        static_world_object.get_materials().len()
+                    );
                     let mut object_material = Material::from_texture(&scene_object.texture);
                     self.textures.insert(scene_object.texture);
                     if scene_object.displace != "".to_string() {
@@ -77,40 +101,40 @@ impl World {
                         self.textures.insert(scene_object.displace);
                     }
                     for material in scene_object.materials {
-                        static_world_object.add_material(Material::from_texture(&material.texture), material.name);
+                        static_world_object
+                            .add_material(Material::from_texture(&material.texture), material.name);
                         self.textures.insert(material.texture);
                     }
                     static_world_object.add_material(object_material, material_name);
                     static_world_object.add_meshes(vertices);
                 } else {
-                    let model_parsed = parse(&scene_object.reference,Transform::zero());
+                    let model_parsed = parse(&scene_object.reference, Transform::zero());
 
-                    let mut object = Object::create(
-                        ObjectType::Mesh,
-                        create_vertices_skinned(&model_parsed.0)
-                    );
+                    let mut object =
+                        Object::create(ObjectType::Mesh, create_vertices_skinned(&model_parsed.0));
 
                     object.set_position(
-                        scene_object.transform.position[0], 
-                        scene_object.transform.position[1], 
-                        scene_object.transform.position[2]
+                        scene_object.transform.position[0],
+                        scene_object.transform.position[1],
+                        scene_object.transform.position[2],
                     );
                     object.set_rotation(
                         scene_object.transform.rotation[0],
                         scene_object.transform.rotation[1],
-                        scene_object.transform.rotation[2]
+                        scene_object.transform.rotation[2],
                     );
                     object.set_scale(
                         scene_object.transform.scale[0],
                         scene_object.transform.scale[1],
-                        scene_object.transform.scale[2]
+                        scene_object.transform.scale[2],
                     );
 
                     if scene_object.skeleton.len() > 0 {
                         let mut skeleton = HashMap::new();
                         for bone_identifier in scene_object.skeleton {
                             for bone in &model_parsed.1 {
-                                let filtered: String = bone.1.2.chars().filter(|c| (*c as u32) >= 32).collect();
+                                let filtered: String =
+                                    bone.1.2.chars().filter(|c| (*c as u32) >= 32).collect();
                                 if filtered == bone_identifier.bone_name {
                                     skeleton.insert(bone_identifier.body_part, bone.1.0);
                                     break;
@@ -120,16 +144,18 @@ impl World {
                         object.set_skeleton(skeleton);
                     }
                     if model_parsed.1.len() > 0 {
-                        object.set_bones(model_parsed.1,
+                        object.set_bones(
+                            model_parsed.1,
                             Vector3::new(0.0, 0.0, 0.0),
                             Vector3::new(0.0, 0.0, 0.0),
-                            Vector3::new(1.0, 1.0, 1.0)
+                            Vector3::new(1.0, 1.0, 1.0),
                         );
                         object.set_object_type(ObjectType::SkinnedMesh);
                     }
 
                     for material in scene_object.materials {
-                        object.add_material(Material::from_texture(&material.texture), material.name);
+                        object
+                            .add_material(Material::from_texture(&material.texture), material.name);
                         self.textures.insert(material.texture);
                     }
                     self.add_object(object);
