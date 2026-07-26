436770dc482c64a86903a477810397627266b369
Charli <123391187+CharliVanNood@users.noreply.github.com>
2026-07-26 02:27:12 +0200

    update a single bone or hierarchy when needed

--- src/renderer/render.rs
+++ src/renderer/render.rs
@@ -657,12 +657,12 @@ impl<'window> Renderer<'window> {
             } else if self.world.get_object(i).get_object_type() == ObjectType::SkinnedMesh {
                 let skeleton = self.world.get_object(i).get_skeleton();
                 self.bones[i][skeleton["head"]].0.rotation.x = -self.player.camera.rotation.x;
-                /*self.bones[i][skeleton["arm_right"]].0.rotation.z = -self.player.camera.rotation.x;
-                self.bones[i][skeleton["head"]].0.rotation.y =
-                    -self.player.camera.rotation.y - 1.57079633;*/
+                //self.bones[i][skeleton["arm_right"]].0.rotation.z = -self.player.camera.rotation.x;
+                /*self.bones[i][skeleton["head"]].0.rotation.y =
+                -self.player.camera.rotation.y - 1.57079633;*/
                 // TODO: make the local character have this dissabled by default.
                 self.bones[i][skeleton["neck"]].0.scale = [0.0, 0.0, 0.0].into();
-                self.update_bones(i);
+                self.update_bone(i, skeleton["head"]);
 
                 let object = self.world.get_object(i);
                 let position = [
@@ -695,6 +695,14 @@ impl<'window> Renderer<'window> {
                     64,
                     bytemuck::cast_slice(normal_ref),
                 );
+            }
+        }
+
+        if self.frame % 20 == 1 {
+            for i in 0..self.world.get_objects().len() {
+                if self.world.get_object(i).get_object_type() == ObjectType::SkinnedMesh {
+                    self.update_bones(i);
+                }
             }
         }
 
@@ -850,6 +858,127 @@ impl<'window> Renderer<'window> {
         }
 
         self.frame += 1;
+    }
+
+    // TODO: Update the bones in hiarchy, this keeps updating O(n)
+    pub fn update_bone(&mut self, object_index: usize, affected_bone: usize) {
+        for (bone_index, bone) in self.bones[object_index].iter().enumerate() {
+            let mut bone_position =
+                Vector3::new(bone.1.position.x, bone.1.position.y, bone.1.position.z);
+
+            let mut is_descendant = false;
+
+            if bone_index == affected_bone {
+                is_descendant = true;
+            }
+
+            if bone.2 != -1 {
+                let mut current_parent = bone.2;
+                for _ in 0..self.bones[object_index].len() {
+                    if current_parent == -1 {
+                        break;
+                    }
+                    if current_parent as usize == affected_bone {
+                        is_descendant = true;
+                    }
+
+                    let current_parent_bone = self.bones[object_index][current_parent as usize];
+
+                    if current_parent_bone.2 != -1 {
+                        bone_position.x += current_parent_bone.1.position.x;
+                        bone_position.y += current_parent_bone.1.position.y;
+                        bone_position.z += current_parent_bone.1.position.z;
+                    }
+
+                    current_parent = current_parent_bone.2;
+                }
+            } else {
+                bone_position = Vector3::new(0.0, 0.0, 0.0);
+            }
+
+            if !is_descendant {
+                continue;
+            }
+
+            let mut global_matrix = create_transforms(
+                [
+                    bone.0.position.x,
+                    bone.0.position.y
+                        + bone_position.y
+                            * self.world.get_object(object_index).get_scale().y
+                            * 150.0,
+                    bone.0.rotation.z,
+                ],
+                bone.0.rotation.into(),
+                bone.0.scale.into(),
+            );
+
+            if bone.2 != -1 {
+                let mut current_parent = bone.2;
+                for _ in 0..self.bones[object_index].len() {
+                    if current_parent == -1 {
+                        break;
+                    }
+
+                    let current_parent_bone = self.bones[object_index][current_parent as usize];
+
+                    let parent_matrix = create_transforms(
+                        current_parent_bone.0.position.into(),
+                        current_parent_bone.0.rotation.into(),
+                        current_parent_bone.0.scale.into(),
+                    );
+                    global_matrix = parent_matrix * global_matrix;
+
+                    current_parent = current_parent_bone.2;
+                }
+            }
+
+            if bone.3 != 0 {
+                let model_mat = transforms::create_transforms(
+                    [
+                        -2.5 + bone_position.x * 0.05,
+                        3.0 + bone_position.y * 0.05,
+                        bone_position.z * 0.05,
+                    ],
+                    [0.0, 0.0, 0.0],
+                    [1.0, 1.0, 1.0],
+                );
+                let normal_mat = (model_mat.invert().unwrap()).transpose();
+
+                let model_ref: &[f32; 16] = model_mat.as_ref();
+                let normal_ref: &[f32; 16] = normal_mat.as_ref();
+
+                self.init.queue.write_buffer(
+                    &self.model_uniform_buffers[bone.3],
+                    0,
+                    bytemuck::cast_slice(model_ref),
+                );
+                self.init.queue.write_buffer(
+                    &self.model_uniform_buffers[bone.3],
+                    64,
+                    bytemuck::cast_slice(normal_ref),
+                );
+            }
+
+            let bind_global = create_transforms(
+                [
+                    0.0,
+                    bone_position.y * self.world.get_object(object_index).get_scale().y * 150.0,
+                    0.0,
+                ],
+                [0.0, 0.0, 0.0],
+                [1.0, 1.0, 1.0],
+            );
+            let inverse_bind = bind_global.invert().expect("BIND DOESN'T EXIST");
+
+            let final_matrix = global_matrix * inverse_bind;
+            self.final_marices[object_index][bone_index] = final_matrix.into();
+        }
+        self.init.queue.write_buffer(
+            &self.bone_buffers[object_index],
+            0,
+            bytemuck::cast_slice(&self.final_marices[object_index]),
+        );
     }
 
     // TODO: Update the bones in hiarchy, this keeps updating O(n)
